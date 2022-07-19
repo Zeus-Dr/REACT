137f3952376226b2e75a71c7be7b8b175527d849
Zeus <mwesigyejosephug0@gmail.com>
2022-07-19 12:35:15 +0300

    List and Keys

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Content from './Content';
-import Footer from './Footer';
-import Header from './Header';
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
   return (
--- src/Content.js
+++ src/Content.js
@@ -1,38 +1,56 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
-    const [name, setName] = useState('Zeus');
-    const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One Bag of Sugar",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "One Bag of Salt",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "One Bag of Rice",
+    },
+  ]);
 
-    const handleNameChange=()=>{
-    const names=['Zeus','Kevin','Dave'];
-    const int=Math.floor(Math.random()*3);
-    setName(names[int]);
-    }
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
 
-    const handleClick = () =>{
-        console.log(count)
-    }
+  return (
+    <main>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label
+              style={item.checked ? { textDecoration: "linethrough" } : null}
+              onDoubleClick={() => handleCheck(item.id)}
+            >
+              {item.item}
+            </label>
+            <FaTrashAlt role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
 
-    const handleClick2 = (name) =>{
-        console.log( name,'was clicked')
-    }
-    const handleClick3 = (e) =>{
-        console.log(e.target.innerText)
-    }
-
-    return (
-        <main>
-            <p onDoubleClick={handleClick}>
-            Hello {name}!
-            </p>
-            <button onClick={handleNameChange}>Change Name</button>
-            <button onClick={() => handleClick2('Zeus')}>Click It</button>
-            <button onClick={(e) => handleClick3(e)}>Click It</button>
-            <button onClick={handleClick}>click me</button>    
-        </main>
-    )
-}
-
-export default Content
+export default Content;
